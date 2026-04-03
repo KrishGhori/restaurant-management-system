9600592f9b608bb0e64966599166f1313d256892
Krish Ghori <krishghori90@gmail.com>
2026-04-03 07:06:16 +0530

    url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api';
+const API_BASE_URL = "https://restaurant-management-system-xi-nine.vercel.app/api"
+const dummy =   import.meta.env.VITE_API_BASE_URL ||  'http://localhost:5000/api';
 
 class ApiService {
   constructor() {
